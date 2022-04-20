092cbadf5b5d14c0885b734893d0ad5321f58c2e
Akanksharma00 <akanksha.uieth@yahoo.com>
2022-04-20 09:36:00 +0530

    Fetching data from API using promise

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,48 +1,41 @@
-import React from 'react';
+import React,{useState} from 'react';
 
 import Button from '../Components/UI/Button';
 
 import style from './Home.module.css';
 
-const movieData = [
-    {
-        date:'Jul 16',
-        name: 'DETROIT,MI',
-        theatre: 'DTE ENERGY MUSIC THEATRE'
-    },{
-        date:'Jul 19',
-        name: 'TORONTO,ON',
-        theatre: 'BUDWEISER STAGE'
-    },{
-        date:'Jul 22',
-        name: 'BRISTOW,VA',
-        theatre: 'JIGGY LUBE LIVE'
-    },{
-        date:'Jul 29',
-        name: 'PHOENIX,AZ',
-        theatre: 'AK-CHIN PAVILION'
-    },{
-        date:'AUG 2',
-        name: 'LAS VEGAS, NV',
-        theatre: 'T-MOBILE ARENA'
-    },{
-        date:'AUG 7',
-        name: 'CONCORD, CA',
-        theatre: 'CONCORD PAVILION'
-    }
-];
-
 const Home = (props) => {
+    const [movies, setMovies] = useState([]);
+
+    const fetchMoviesHandler = () => {
+        fetch('https://swapi.dev/api/films/')
+            .then(response => {
+                return response.json();
+            })
+            .then(data => {
+                const transformedMovies = data.results.map(movieData => {
+                    return {
+                        id: movieData.episode_id,
+                        date: movieData.release_date,
+                        title: movieData.title,
+                        directorName : movieData.director,
+                    };
+                });
+                setMovies(transformedMovies);
+            });
+    };
+
     return (
         <section>
+            <button onClick={fetchMoviesHandler}>Fetch Movies</button>
             <h2 className={style.heading}>TOUR</h2>
             <table className={style.table}>
-                {movieData.map(data => {
+                {movies.map(data => {
                     return (
                     <tr className={style.tableRow}>
                         <td className={style.tableCol}>{data.date}</td>
-                        <td className={style.tableCol}>{data.name}</td>
-                        <td className={style.tableCol}>{data.theatre}</td>
+                        <td className={style.tableCol}>{data.title}</td>
+                        <td className={style.tableCol}>{data.directorName}</td>
                         <td className={style.tableCol}><Button>BUY TICKETS</Button></td>
                     </tr>)
                 })}
